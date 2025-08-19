dfb99483967c33939848b4e39ee1889aa29d9ab5
crist <crigsore1@gmail.com>
2025-08-19 09:28:59 -0300

    Adding classes generated from StarUML Creating main loop Connecting Menu and Game classes

---
+++ code/Menu.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from pygame.font import Font
+import pygame.image
+from pygame import Surface, Rect
+
+from code.Const import WIN_WIDTH, YELLOW
+
+
+class Menu:
+    def __init__(self, window):
+        self.window = window
+        self.surf = pygame.image.load('./asset/MenuBg2.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
+
+    def run(self, ):
+        pygame.mixer_music.load('./asset/Menu.wav')
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(text_size=90, text="BATTLEFIELD", text_color=YELLOW, text_center_pos=((WIN_WIDTH / 2), 120))
+            pygame.display.flip()
+
+            # Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Close Window
+                    quit()  # End pygame
+
+    def menu_text(self,text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Stencil", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
+
+
+
