b96f3c1f2a081505fb2e8da475ada4aec3112c44
crist <crigsore1@gmail.com>
2025-08-19 21:09:35 -0300

    Configuring the menu Configuring the files Background, Game, Entity and EntityFactory

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+
+from code.Background import Background
+
 
 class EntityFactory:
-    def __init__(self):
-        pass
 
-    def get_entity(self, entity_type):
-        pass
+    @staticmethod
+    def get_entity(entity_name: 'str', position=(0,0)):
+        match entity_name:
+            case 'BG1':
+                list_bg = []
+                for i in range(5):
+                    list_bg.append(Background(f'BG1{i}', (0,0)))
+                return list_bg
+        return None
--- code/Menu.py
+++ code/Menu.py
@@ -12,15 +12,22 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
+        menu_option = 0
         pygame.mixer_music.load('./asset/Menu.wav')
         pygame.mixer_music.play(-1)
         while True:
+            # DRAW IMAGES
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(text_size=120, text="BATTLEFIELD", text_color=YELLOW, text_center_pos=((WIN_WIDTH / 2), 120))
+            self.menu_text(text_size=120, text="BATTLEFIELD", text_color=YELLOW,
+                           text_center_pos=((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(text_size=40, text=MENU_OPTION[i], text_color=WHITE,text_center_pos=((WIN_WIDTH / 2), 320 + 60 * i))
-
+                if i == menu_option:
+                    self.menu_text(text_size=40, text=MENU_OPTION[i], text_color=YELLOW,
+                                   text_center_pos=((WIN_WIDTH / 2), 320 + 60 * i))
+                else:
+                    self.menu_text(text_size=40, text=MENU_OPTION[i], text_color=WHITE,
+                                   text_center_pos=((WIN_WIDTH / 2), 320 + 60 * i))
             pygame.display.flip()
 
             # Check for all events
@@ -28,6 +35,19 @@ class Menu:
                 if event.type == pygame.QUIT:
                     pygame.quit()  # Close Window
                     quit()  # End pygame
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN: # DOWN KEY
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pygame.K_UP: # UP KEY
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+                    if event.type == pygame.K_RETURN: # ENTER
+                        return MENU_OPTION[menu_option]
 
     def menu_text(self,text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Stencil", size=text_size)
